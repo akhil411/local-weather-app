e0da3923768d8a084a3e34375142d9d988ac5b51
Akhil <akhilvijayan004@gmail.com>
2020-03-15 18:50:35 +1100

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # local-weather-app
+
+## This project is created with [Vue-CLI]
+
+## To set up project
+
+### Unzip the files
+### Open terminal and navigate inside the project folder
+### run command "npm install"
+### then run "npm run serve"
+### Application will be running on http://localhost:8080 /
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Weather />
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+body, p {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
